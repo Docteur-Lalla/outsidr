5581791fa5968cdc789e931bd836581c40ee92df
Jordan Maheu <jmaheu1995@gmail.com>
2017-09-15 08:33:20 +0200

    add the singleton of EntityManager

---
+++ src/main/java/fr/istic/m2/EntityManagerHelper.java
@@ -0,0 +1,61 @@
+package fr.istic.m2;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
+
+public class EntityManagerHelper {
+
+    private static final EntityManagerFactory emf;
+    private static final ThreadLocal<EntityManager> threadLocal;
+
+    static {
+
+        EntityManagerFactory tempEntityManager;
+
+        try {
+            tempEntityManager = Persistence.createEntityManagerFactory("dev");
+        } catch (PersistenceException e) {
+            tempEntityManager = Persistence.createEntityManagerFactory("devMySQL");
+        }
+
+        emf = tempEntityManager;
+        threadLocal = new ThreadLocal<EntityManager>();
+    }
+
+    public static EntityManager getEntityManager() {
+        EntityManager em = threadLocal.get();
+
+        if (em == null) {
+            em = emf.createEntityManager();
+            threadLocal.set(em);
+        }
+        return em;
+    }
+
+    public static void closeEntityManager() {
+        EntityManager em = threadLocal.get();
+        if (em != null) {
+            em.close();
+            threadLocal.set(null);
+        }
+    }
+
+    public static void closeEntityManagerFactory() {
+        emf.close();
+    }
+
+    public static void beginTransaction() {
+        getEntityManager().getTransaction().begin();
+    }
+
+    public static void rollback() {
+        getEntityManager().getTransaction().rollback();
+    }
+
+    public static void commit() {
+        getEntityManager().getTransaction().commit();
+    }
+}
+

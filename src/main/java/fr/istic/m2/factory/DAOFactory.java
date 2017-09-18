34af62598ed8f45f0ba1082264e15f3aee0f0f5a
Kévin Le Bon <kevin.le_bon@yahoo.fr>
2017-09-18 12:03:18 +0000

    Fix the DAO factory and add a dummy REST server

--- src/main/java/fr/istic/m2/factory/DAOFactory.java
+++ src/main/java/fr/istic/m2/factory/DAOFactory.java
@@ -4,45 +4,44 @@ import fr.istic.m2.dao.*;
 import fr.istic.m2.entities.*;
 
 public class DAOFactory {
+  private static UserDAO userDAO;
+  private static ActivityDAO activityDAO;
+  private static LocationDAO locationDAO;
+  private static MeteoDAO meteoDAO;
+  private static RegistrationDAO registrationDAO;
 
-    private static UserDAO userDAO;
-    private static ActivityDAO activityDAO;
-    private static LocationDAO locationDAO;
-    private static MeteoDAO meteoDAO;
-    private static RegistrationDAO registrationDAO;
-
-    public static DAO<User> getUserDAO(){
-        if(userDAO == null){
-            return userDAO = new UserDAO();
-        }
-        return userDAO;
+  public static DAO<User> getUserDAO() {
+    if (userDAO == null) {
+      return userDAO = new UserDAO();
     }
+    return userDAO;
+  }
 
-    public static DAO<Activity> getActivityDAO(){
-        if(activityDAO == null){
-            return activityDAO = new ActivityDAO();
-        }
-        return userDAO;
+  public static DAO<Activity> getActivityDAO() {
+    if (activityDAO == null) {
+      return activityDAO = new ActivityDAO();
     }
+    return userDAO;
+  }
 
-    public static DAO<Location> getLocatonDAO(){
-        if(locationDAO == null){
-            return locationDAO = new LocationDAO();
-        }
-        return activityDAO;
+  public static DAO<Location> getLocatonDAO() {
+    if (locationDAO == null) {
+      return locationDAO = new LocationDAO();
     }
+    return activityDAO;
+  }
 
-    public static DAO<Meteo> getMeteoDAO(){
-        if(meteoDAO == null){
-            return meteoDAO = new MeteoDAO();
-        }
-        return meteoDAO;
+  public static DAO<Meteo> getMeteoDAO() {
+    if (meteoDAO == null) {
+      return meteoDAO = new MeteoDAO();
     }
+    return meteoDAO;
+  }
 
-    public static DAO<Registration> getRegistrationDAO(){
-        if(registrationDAO == null){
-            return registrationDAO = new RegistrationDAO();
-        }
-        return registrationDAO;
+  public static DAO<Registration> getRegistrationDAO() {
+    if (registrationDAO == null) {
+      return registrationDAO = new RegistrationDAO();
     }
+    return registrationDAO;
+  }
 }
---
+++ src/main/java/fr/istic/m2/jaxrs/RestServer.java
@@ -0,0 +1,11 @@
+package fr.istic.m2.jaxrs;
+
+import java.util.logging.Logger;
+
+public class RestServer {
+  private static final Logger logger = Logger.getLogger(RestServer.class.getName());
+
+  public static void main(String[] args) {
+
+  }
+}

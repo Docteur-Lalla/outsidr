a5c063c41fe5402f68e79be99fc79c35e36b440d
Kévin Le Bon <kevin.le_bon@yahoo.fr>
2017-09-18 10:49:57 +0000

    Add the javadoc for the CompositeKey and Registration classes

--- src/main/java/fr/istic/m2/entities/Registration.java
+++ src/main/java/fr/istic/m2/entities/Registration.java
@@ -3,36 +3,69 @@ package fr.istic.m2.entities;
 import javax.persistence.*;
 import java.io.Serializable;
 
+/**
+ * Class embedding the composite key for the Registration entity.
+ * @see Registration
+ */
 class CompositeKey implements Serializable {
   private User user;
   private Activity activity;
 
+  /**
+   * Default constructor
+   */
   public CompositeKey() {
 
   }
 
+  /**
+   * Constructor with a parameter for each field of the class.
+   * @param user the user
+   * @param activity the activity the user is registered in
+   */
   public CompositeKey(User user, Activity activity) {
     this.user = user;
     this.activity = activity;
   }
 
+  /**
+   * Getter of the user.
+   * @return the user
+   */
   public User getUser() {
     return user;
   }
 
+  /**
+   * Setter of the user.
+   * @param user the user
+   */
   public void setUser(User user) {
     this.user = user;
   }
 
+  /**
+   * Getter of the activity.
+   * @return the activity
+   */
   public Activity getActivity() {
     return activity;
   }
 
+  /**
+   * Setter of the activity.
+   * @param activity the activity
+   */
   public void setActivity(Activity activity) {
     this.activity = activity;
   }
 }
 
+/**
+ * Class representing the association of a user and an activity in the database.
+ * @see User
+ * @see Activity
+ */
 @Entity
 @IdClass(CompositeKey.class)
 public class Registration {
@@ -40,10 +73,19 @@ public class Registration {
   private Activity activity;
   private String level;
 
+  /**
+   * Default constructor
+   */
   public Registration() {
 
   }
 
+  /**
+   * Constructor with a parameter for each field in the class.
+   * @param user the user
+   * @param activity the activity
+   * @param level the level of skill the user wishes to practice at
+   */
   public Registration(User user, Activity activity, String level) {
     this.user = user;
     this.activity = activity;
@@ -51,30 +93,54 @@ public class Registration {
   }
 
 
+  /**
+   * Getter of the user.
+   * @return the user
+   */
   @ManyToOne
   @Id
   public User getUser() {
     return user;
   }
 
+  /**
+   * Setter of the user.
+   * @param user the user
+   */
   public void setUser(User user) {
     this.user = user;
   }
 
+  /**
+   * Getter of the activity.
+   * @return the activity
+   */
   @ManyToOne
   @Id
   public Activity getActivity() {
     return activity;
   }
 
+  /**
+   * Setter of the activity.
+   * @param activity the activity
+   */
   public void setActivity(Activity activity) {
     this.activity = activity;
   }
 
+  /**
+   * Getter of the skill level.
+   * @return the skill level the user wishes to practice at
+   */
   public String getLevel() {
     return level;
   }
 
+  /**
+   * Setter of the skill level.
+   * @param level the skill level the user wishes to practice at
+   */
   public void setLevel(String level) {
     this.level = level;
   }

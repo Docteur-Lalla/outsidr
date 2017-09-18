c70f9f33f9137b207b34e1fff31963a10b7bf4d6
Kévin Le Bon <kevin.le_bon@yahoo.fr>
2017-09-18 11:11:12 +0000

    Add the javadoc for the Location class

--- src/main/java/fr/istic/m2/entities/Location.java
+++ src/main/java/fr/istic/m2/entities/Location.java
@@ -6,6 +6,10 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
+/**
+ * Class representing locations where the activities can take place in the database.
+ * @see Activity
+ */
 @Entity
 public class Location {
   private int id;
@@ -14,57 +18,107 @@ public class Location {
   private Activity activity;
   private Meteo meteo;
 
+  /**
+   * Default constructor.
+   */
   public Location() {
 
   }
 
-  public Location(int id, String name, String address, Meteo meteo) {
-    this.id = id;
+  /**
+   * Constructor with a parameter for each field in the class.
+   * @param name the name given to the location
+   * @param address the address
+   * @param activity the activity associated with the location
+   * @param meteo the meteo expected during the next week-end
+   */
+  public Location(String name, String address, Activity activity, Meteo meteo) {
     this.name = name;
     this.address = address;
+    this.activity = activity;
     this.meteo = meteo;
   }
 
+  /**
+   * Getter of the ID.
+   * @return the ID of the location in the database
+   */
   @Id
   @GeneratedValue
   public int getId() {
     return id;
   }
 
+  /**
+   * Setter of the ID.
+   * @param id the ID of the location in the database
+   */
   public void setId(int id) {
     this.id = id;
   }
 
+  /**
+   * Getter of the name.
+   * @return the name given to the location
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Setter of the name.
+   * @param name the name given to the location
+   */
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * Getter of the address.
+   * @return the address
+   */
   public String getAddress() {
     return address;
   }
 
+  /**
+   * Setter of the address.
+   * @param address the address
+   */
   public void setAddress(String address) {
     this.address = address;
   }
 
+  /**
+   * Getter of the activity.
+   * @return the activity associated with the location
+   */
   @ManyToOne
   public Activity getActivity() {
     return activity;
   }
 
+  /**
+   * Setter of the activity.
+   * @param activity the activity associated with the location
+   */
   public void setActivity(Activity activity) {
     this.activity = activity;
   }
 
+  /**
+   * Getter of the meteo.
+   * @return the meteo expected the next week-end
+   */
   @OneToOne
   public Meteo getMeteo() {
     return meteo;
   }
 
+  /**
+   * Setter of the meteo.
+   * @param meteo the meteo expected the next week-end
+   */
   public void setMeteo(Meteo meteo) {
     this.meteo = meteo;
   }

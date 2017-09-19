71aa7d87977d17450699a1bd58a9aab74c2e1c03
Jordan Maheu <jmaheu1995@gmail.com>
2017-09-19 15:16:38 +0200

    Add a first implementation of TestDAO classes.

--- src/test/java/fr/istic/m2/dao/TestRegistrationDAO.java
+++ src/test/java/fr/istic/m2/dao/TestRegistrationDAO.java
@@ -2,5 +2,25 @@ package fr.istic.m2.dao;
 
 import junit.framework.TestCase;
 
-public class TestRegistrationDAO extends TestCase{
+public class TestRegistrationDAO extends TestCase {
+
+    public void testFindAll() {
+        assert true;
+    }
+
+    public void testFindOne() {
+        assert true;
+    }
+
+    public void testCreate() {
+        assert true;
+    }
+
+    public void testUpdate() {
+        assert true;
+    }
+
+    public void testDelete() {
+        assert true;
+    }
 }
--- src/test/java/fr/istic/m2/dao/TestUserDAO.java
+++ src/test/java/fr/istic/m2/dao/TestUserDAO.java
@@ -3,4 +3,24 @@ package fr.istic.m2.dao;
 import junit.framework.TestCase;
 
 public class TestUserDAO extends TestCase {
+
+    public void testFindAll() {
+        assert true;
+    }
+
+    public void testFindOne() {
+        assert true;
+    }
+
+    public void testCreate() {
+        assert true;
+    }
+
+    public void testUpdate() {
+        assert true;
+    }
+
+    public void testDelete() {
+        assert true;
+    }
 }
